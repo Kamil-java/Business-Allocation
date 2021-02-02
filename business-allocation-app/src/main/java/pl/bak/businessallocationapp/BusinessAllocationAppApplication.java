3f40cdc6638d935926bdac28b9d53eac0c3392cd
KokosJava <kamil.slowik@vp.pl>
2021-02-02 11:39:49 +0100

    Initial commit

---
+++ business-allocation-app/src/main/java/pl/bak/businessallocationapp/BusinessAllocationAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bak.businessallocationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAllocationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessAllocationAppApplication.class, args);
+    }
+
+}
---
+++ business-allocation-app/src/test/java/pl/bak/businessallocationapp/BusinessAllocationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bak.businessallocationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAllocationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
